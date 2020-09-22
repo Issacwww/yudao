88570b5acef88ca61f1e61fe9b161357972f7a25
Issacwww <siw32@pitt.edu>
2020-09-21 20:04:32 -0700

    migrate to mac

--- sql/mockData.sql
+++ sql/mockData.sql
@@ -1,13 +1,29 @@
--- INSERT INTO yudao.store (store_name, address, phone)
--- VALUES (
---     '旗舰店',
---     '中国云南省香格里拉人民东路1号',
---     '1235324994'
---   );
+INSERT INTO public.store (store_name, address, phone,open_date)
+VALUES (
+    '旗舰店',
+    '中国云南省香格里拉人民东路1号',
+    '1235324994',
+    '2020-1-1'
+  );
 
--- INSERT INTO yudao.store (store_name, address, phone)
--- VALUES (
---     '二号店',
---     '中国云南省香格里拉解放西路9号',
---     '19935324994'
---   );
+INSERT INTO public.store (store_name, address, phone, open_date)
+VALUES (
+    '二号店',
+    '中国云南省香格里拉解放西路9号',
+    '19935324994',
+    '2020-9-21'
+  );
+
+INSERT INTO public.admin(admin_name, password, is_super_admin)
+	VALUES ('Admin', 'pwd', true);
+
+INSERT INTO public.admin(admin_name, password, is_super_admin)
+	VALUES ('store1Admin', 'pwd', false);
+	
+	INSERT INTO public.admin(admin_name, password, is_super_admin)
+	VALUES ('store2Admin', 'pwd', false);
+
+INSERT INTO public.credential( admin_id, store_id)
+	VALUES ( 2, 1);
+INSERT INTO public.credential( admin_id, store_id)
+	VALUES ( 3, 2);
